cfffddb05dac5793576d57044d16e8bd07c44a01
dealsky <dealsky000@outlook.com>
2018-03-23 09:50:44 +0800

    init

--- src/api/config.js
+++ src/api/config.js
@@ -4,10 +4,10 @@ export const commonParams = {
   nCharset: 'utf-8',
   outCharset: 'utf-8',
   notice: 0
-};
+}
 
 export const options = {
   param: 'jsonpCallback'
-};
+}
 
-export const ERR_OK = 0;
+export const ERR_OK = 0

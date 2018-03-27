fd7b9a99b6a3be329bb5da307ff1ab5ce1bcfe97
dealsky <dealsky000@outlook.com>
2018-03-27 19:47:16 +0800

    init

--- src/store/state.js
+++ src/store/state.js
@@ -1,5 +1,19 @@
+import {playMode} from 'common/js/config'
+
 const state = {
-  singer: {}
+  singer: {},
+  // 播放器播放状态
+  playing: false,
+  // 播放器全屏状态
+  fullScreen: false,
+  // 播放列表
+  playList: [],
+  // 播放顺序列表
+  sequenceList: [],
+  // 播放模式
+  mode: playMode.sequence,
+  // 当前播放的索引
+  currentIndex: -1
 }
 
 export default state

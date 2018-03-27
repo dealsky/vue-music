6ccc6a142592a424e0771c6dad1da7b1c4dfe6a6
dealsky <dealsky000@outlook.com>
2018-03-27 14:11:26 +0800

    init

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -1,6 +1,6 @@
 <template>
   <transition name="slide">
-    <div class="singer-detail"></div>
+    <music-list :songs="songs" :title="title" :bg-image="bgImage"></music-list>
   </transition>
 </template>
 
@@ -9,6 +9,7 @@
   import {getSingerDetail} from 'api/singer'
   import {ERR_OK} from 'api/config'
   import {createSong} from 'common/js/song'
+  import MusicList from 'components/music-list/music-list'
 
   export default {
     data() {
@@ -22,7 +23,13 @@
     computed: {
       ...mapGetters([
         'singer'
-      ])
+      ]),
+      title() {
+        return this.singer.name
+      },
+      bgImage() {
+        return this.singer.avatar
+      }
     },
     methods: {
       _getDetail() {
@@ -46,21 +53,15 @@
         })
         return ret
       }
+    },
+    components: {
+      MusicList
     }
   }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
   @import "~common/stylus/variable"
-
-  .singer-detail
-    position: fixed
-    z-index: 100
-    top: 0
-    left: 0
-    right: 0
-    bottom: 0
-    background: $color-background
 
   .slide-enter-active, .slide-leave-active
     transition: all 0.3s

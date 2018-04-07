3ce8cb0c4d7db47630122ba60dd953d691a28956
dealsky <dealsky000@outlook.com>
2018-04-07 23:29:14 +0800

    init

--- src/api/recommend.js
+++ src/api/recommend.js
@@ -37,3 +37,26 @@ export function getDistList() {
     return Promise.resolve(res.data)
   })
 }
+
+export function getSongList(dissId) {
+  const url = 'api/getSongList'
+
+  const data = Object.assign({}, commonParams, {
+    type: 1,
+    json: 1,
+    utf8: 1,
+    onlysong: 0,
+    disstid: dissId,
+    loginUin: 0,
+    hostUin: 0,
+    platform: 'yqq',
+    needNewCode: 0,
+    format: 'json'
+  })
+
+  return axios.get(url, {
+    params: data
+  }).then((res) => {
+    return Promise.resolve(res.data)
+  })
+}
--- src/components/disc/disc.vue
+++ src/components/disc/disc.vue
@@ -1,14 +1,25 @@
 <template>
   <transition name="slide">
-    <music-list :title="title" :bg-image="bgImage"></music-list>
+    <music-list :title="title" :bg-image="bgImage" :songs="songs"></music-list>
   </transition>
 </template>
 
 <script>
   import MusicList from 'components/music-list/music-list'
   import {mapGetters} from 'vuex'
+  import {getSongList} from 'api/recommend'
+  import {ERR_OK} from 'api/config'
+  import {createSong} from 'common/js/song'
 
   export default {
+    data() {
+      return {
+        songs: []
+      }
+    },
+    created() {
+      this._getSongList()
+    },
     computed: {
       title() {
         return this.disc.dissname
@@ -20,7 +31,31 @@
         'disc'
       ])
     },
-    components: {MusicList}
+    methods: {
+      _getSongList() {
+        if (!this.disc.dissid) {
+          this.$router.push('/recommend')
+          return
+        }
+        getSongList(this.disc.dissid).then((res) => {
+          if (res.code === ERR_OK) {
+            this.songs = this._normalizeSongs(res.cdlist[0].songlist)
+          }
+        })
+      },
+      _normalizeSongs(list) {
+        let ret = []
+        list.forEach((musicData) => {
+          if (musicData.songid && musicData.albumid) {
+            ret.push(createSong(musicData))
+          }
+        })
+        return ret
+      }
+    },
+    components: {
+      MusicList
+    }
   }
 </script>
 

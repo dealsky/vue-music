09a31af5e8674fe099c32559ee7b4c2fc0d9cfaf
dealsky <dealsky000@outlook.com>
2018-04-08 17:39:17 +0800

    init

--- src/api/search.js
+++ src/api/search.js
@@ -12,3 +12,27 @@ export function getHotKey() {
 
   return jsonp(url, data, options)
 }
+
+export function search(query, page, zhida) {
+  const url = 'https://c.y.qq.com/soso/fcgi-bin/search_for_qq_cp'
+
+  const data = Object.assign({}, commonParams, {
+    uin: 0,
+    platform: 'h5',
+    needNewCode: 1,
+    w: query,
+    zhidaqu: 1,
+    catZhida: zhida ? 1 : 0,
+    t: 0,
+    flag: 1,
+    ie: 'utf-8',
+    sem: 1,
+    aggr: 0,
+    perpage: 20,
+    n: 20,
+    p: page,
+    remoteplace: 'txt.mqq.all'
+  })
+
+  return jsonp(url, data, options)
+}

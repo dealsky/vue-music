c5894b0633fd11d28e433060e4d2ccf2bf6f2329
dealsky <dealsky000@outlook.com>
2018-04-13 14:07:58 +0800

    添加播放列表组件，实现了它的显示和隐藏

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -89,11 +89,12 @@
             <i @click.stop.prevent="togglePlaying" class="icon-mini" :class="miniIcon"></i>
           </progress-circle>
         </div>
-        <div class="control">
+        <div class="control" @click.stop="showPlaylist">
           <i class="icon-playlist"></i>
         </div>
       </div>
     </transition>
+    <playlist ref="playlist"></playlist>
     <audio ref="audio" :src="currentSong.url" @canplay="ready"
            @error="error"
            @timeupdate="updateTime"
@@ -111,6 +112,7 @@
   import {shuffle} from 'common/js/util'
   import Lyric from 'lyric-parser'
   import Scroll from 'base/scroll/scroll'
+  import Playlist from 'components/playlist/playlist'
 
   const transform = prefixStyle('transform')
   const transitionDuration = prefixStyle('transitionDuration')
@@ -381,6 +383,9 @@
         this.$refs.middleL.style.opacity = opacity
         this.$refs.middleL.style[transitionDuration] = `${time}ms`
       },
+      showPlaylist() {
+        this.$refs.playlist.show()
+      },
       _pad(num, n = 2) {
         let len = num.toString().length
         while (len < n) {
@@ -433,7 +438,8 @@
     components: {
       Scroll,
       ProgressBar,
-      ProgressCircle
+      ProgressCircle,
+      Playlist
     }
   }
 </script>
